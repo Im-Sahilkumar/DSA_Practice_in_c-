9e3d6180836f3d05e452bfacd0a826a551a9ddf9
Sahil Kumar <143871621+Sahilkumar70@users.noreply.github.com>
2026-01-07 15:25:13 +0530

    5or6commit

---
+++ 5.cpp
@@ -0,0 +1,12 @@
+// find the sum of two number?
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"input a first no"<<endl;
+    cin>>a;
+    cout<<"input a second no"<<endl;
+    cin>>b;
+    int sum = a+b;
+    cout<<"the sum of two no is"<<sum<<endl;
+}
---
+++ 6.cpp
@@ -0,0 +1,11 @@
+//calculate the simple intreset?
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    float avg;
+ cout<<"input a no"<<endl;
+ cin>>a>>b>>c;
+ avg = (a+b+c)/3;
+ cout<<"the average is"<<avg<<endl;
+}
